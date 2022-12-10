e68136335b2ae42314390475ebb875e1039c8b6f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 07:47:00 +0000

    Adding tarfile member sanitization to extractall()

--- load_data.py
+++ load_data.py
@@ -274,7 +274,26 @@ class LoadAtariHeadData:
 
                         elif filename.endswith('.tar.bz2'):
                             with tarfile.open(extract_dir.name + "/" + filename, 'r') as tar_archive:
-                                tar_archive.extractall(extract_dir.name)
+                                def is_within_directory(directory, target):
+                                    
+                                    abs_directory = os.path.abspath(directory)
+                                    abs_target = os.path.abspath(target)
+                                
+                                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                                    
+                                    return prefix == abs_directory
+                                
+                                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                                
+                                    for member in tar.getmembers():
+                                        member_path = os.path.join(path, member.name)
+                                        if not is_within_directory(path, member_path):
+                                            raise Exception("Attempted Path Traversal in Tar File")
+                                
+                                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                    
+                                
+                                safe_extract(tar_archive, extract_dir.name)
                                 self._update_png_name_df(tar_archive.getnames())
                         print('%s has been processed.' %(filename))
                         
